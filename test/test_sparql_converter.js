811d64ba09a6c514e17f830bc30069a618c7854e
Your Name <ryachen@nuevaschool.org>
2019-07-20 10:34:29 -0700

    added join support

--- test/test_sparql_converter.js
+++ test/test_sparql_converter.js
@@ -24,14 +24,28 @@ async function main() {
     `,
     `
     // Filter for person who was drafted by the cavs and won the MVP award
-    now => [athlete] of @org.wikidatasportsskill.athlete(), P647 == "Cleveland Cavaliers" && P166 == "NBA Most Valuable Player Award" => notify;
+    now => [athlete] of @org.wikidatasportsskill.athlete(),
+    P647 == "Q162990"^^org.wikidatasportsskill:sports_teams("Cleveland Cavaliers")
+    && P166 == ["Q222047"^^org.wikidatasportsskill:award_received("NBA Most Valuable Player Award")] => notify;
     `,
     `
-    // Filter for person who has played for the
-    now => [athlete] of @org.wikidatasportsskill.athlete(), P54 == ["LAL"^^org.wikidatasportsskill:sports_teams("Los Angeles Lakers"), "GSW"^^org.wikidatasportsskill:sports_teams("Golden State Warriors")] => notify;
+    // Filter for person who has played for the Lakers and Warriors
+    now => [athlete] of @org.wikidatasportsskill.athlete(),
+    P54 == ["Q121783"^^org.wikidatasportsskill:sports_teams("Los Angeles Lakers"),
+    "Q157376"^^org.wikidatasportsskill:sports_teams("Golden State Warriors")] => notify;
+    `,
+    `
+    now => (([sports_team, P286] of @org.wikidatasportsskill.sports_team(),
+    P286 == "Q523630"^^org.wikidata:human('Steve Kerr'))
+    join ([athlete, P647] of @org.wikidatasportsskill.athlete())), P647 == sports_team => notify;
     `
   ];
-  const answers = ["Stephen Curry", "LeBron James", "Wilt Chamberlain"];
+  const answers = [
+    "Stephen Curry",
+    "LeBron James",
+    "Wilt Chamberlain",
+    "Klay Thompson"
+  ];
 
   Promise.all(
     code.map((code) => {
@@ -39,13 +53,24 @@ async function main() {
         code = code.trim();
         AppGrammar.parseAndTypecheck(code, _schemaRetriever).then((program) => {
           const sparqlQuery = SparqlConverter.program_to_sparql(program);
+          //if there is a join
+          if (sparqlQuery[1]) {
+            SparqlQuery.query(sparqlQuery[0]).then((response) => {
+              let query_output =
+                response["results"]["bindings"][0]["v2Label"]["value"];
 
-          SparqlQuery.query(sparqlQuery).then((response) => {
-            let query_output =
-              response["results"]["bindings"][0]["personLabel"]["value"];
+              resolve(query_output);
+            });
 
-            resolve(query_output);
-          });
+            //there is no join
+          } else {
+            SparqlQuery.query(sparqlQuery[0]).then((response) => {
+              let query_output =
+                response["results"]["bindings"][0]["vLabel"]["value"];
+
+              resolve(query_output);
+            });
+          }
         });
       });
       return promise;
